42b98876c043d05e30417784a80c0b4b1f6cc214
Nandita <carlos@dynetv.com>
2018-11-12 12:07:48 -0300

    ULD: v0.1 wip 03

--- develop/Update Loaders Disk.py
+++ develop/Update Loaders Disk.py
@@ -1,11 +1,40 @@
+# v0.1 wip 03
+
+import os
+
+# new disk to use
+disk = 'D:'
+
+# get main tools list
 toollist = comp.GetToolList().values()
 
-path = 'C:\\Users\\Nandita\\Desktop\\test.jpg'
-
+# find loaders nodes
 for tool in toollist:
     if tool.GetAttrs()["TOOLS_RegID"] == 'Loader':
-        print tool.GetAttrs()['TOOLS_Name']
-        tool.SetAttrs({'TOOLS_Name' : 'name_test_01'})
-        #tool.SetAttrs({'TOOLST_Clip_Name' : path})
+        # get original times
+        globalIn = tool.GlobalIn[comp.CurrentTime]
+        globalOut = tool.GlobalIn[comp.CurrentTime]
+        trimIn = tool.ClipTimeStart[comp.CurrentTime]
+        trimOut = tool.ClipTimeEnd[comp.CurrentTime]
+
+        # get loader path
+        loaderPath = tool.GetAttrs("TOOLST_Clip_Name")
+        loaderPathClean = loaderPath[1]
+
+        # split original loader path
+        loaderPathSplit = loaderPathClean.split(os.sep)
+
+        # added new disk
+        new_disk = loaderPathSplit[0] = disk + '\\'
+
+        # join new path
+        path = os.path.join(*loaderPathSplit)
+
+        # replace loader path
         tool.Clip = path
-        print True
+
+        # update loader times
+        tool.GlobalIn = globalIn
+        tool.GlobalOut = globalOut
+        tool.ClipTimeStart = trimIn
+        tool.ClipTimeEnd = trimOut
